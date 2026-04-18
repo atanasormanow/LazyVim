1886f61cb79e2459a487827b698f3237563b0352
Atanas Ormanov <atanasormanow@gmail.com>
2026-04-18 16:27:09 +0300

    Open the snacks.nvim explorer to the right

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -10,4 +10,18 @@ return {
 
   -- easy preview for markdown files
   { "iamcco/markdown-preview.nvim" },
+
+  -- position the snacks explorer on the right
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            layout = { preset = "right" },
+          },
+        },
+      },
+    },
+  },
 }
