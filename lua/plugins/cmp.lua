50717bd73c1542bfcfa664335720f0d264cf36bf
Atanas Ormanov <atanasormanow@gmail.com>
2026-04-19 01:14:18 +0300

    Change autocompletion keymaps

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,17 @@
+-- Autocompletion
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      -- Keep the "enter" preset (Enter still works)
+      preset = "enter",
+
+      -- Navigate suggestions with CTRL+J/K
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+
+      -- accept suggestion
+      ["<C-l>"] = { "select_and_accept" },
+    },
+  },
+}
