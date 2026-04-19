3500fc250593cb4d844daa792bc7ca339aad949d
Atanas Ormanov <atanasormanow@gmail.com>
2026-04-19 14:26:04 +0300

    Add useful keymaps and options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,25 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- NOTE: Keymaps starting with built-in operators (d, c, y, v, etc.)
+-- won't show in WhichKey because Vim enters operator-pending mode
+-- before WhichKey can intercept.
+
+-- Deleta a line without the newline
+vim.keymap.set("n", "dl", "0d$", { desc = "Delete line content" })
+
+-- navigate/move buffers in BufferLine
+vim.keymap.set("n", "gj", "<cmd>BufferLineCyclePrev<cr>", { desc = "Buffer (prev)" })
+vim.keymap.set("n", "gk", "<cmd>BufferLineCycleNext<cr>", { desc = "Buffer (next)" })
+vim.keymap.set("n", "g<", "<cmd>BufferLineMovePrev<cr>", { desc = "Buffer move (left)" })
+vim.keymap.set("n", "g>", "<cmd>BufferLineMoveNext<cr>", { desc = "Buffer move (right)" })
+
+-- easier jump between matching braces
+vim.keymap.set("", "<tab>", "%")
+
+-- "exit anyways"
+vim.keymap.set("n", "q:", "<cmd>q<return>")
+
 -- Treesitter incremental selection (using Neovim's built-in functions)
 -- Note: Can't use `an`/`in` directly because mini.ai overrides them
 --
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- basic bg phonetic layout
+vim.opt.langmap = "чявертъуиопшщасдфгхйклзьцжбнмЧЯВЕРТЪУИОПШЩАСДФГХЙКЛЗѝЦЖБНМ;`qwertyuiop[]asdfghjklzxcvbnm~QWERTYUIOP{}ASDFGHJKLZXCVBNM,ю\\,Ю\\|,"
+
+-- Disable autoformat on save
+vim.g.autoformat = false
+
+-- Do NOT insert comment on "o"/"O"
+vim.opt.formatoptions:remove({ "o" })
