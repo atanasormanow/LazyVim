1f93960a2113f25ed1b9987f222c4b2b50375fbc
Atanas Ormanov <atanasormanow@gmail.com>
2026-04-19 23:45:53 +0300

    Change the color of flash.nvim markers

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,3 +1,11 @@
+-- Set jump marker colors when the colorscheme is applied
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("flash_colors", { clear = true }),
+  callback = function()
+    vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#000000", bg = "#00ff00", bold = true })
+  end,
+})
+
 return {
   "folke/flash.nvim",
   keys = {
