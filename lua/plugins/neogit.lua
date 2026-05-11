83a0f8dc31421ca9a2cf4f1b099cbe77f01447fc
Atanas Ormanov <atanasormanow@gmail.com>
2026-05-11 15:19:22 +0300

    Improve diff highlights; Add sshfs

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,15 +1,35 @@
 -- powerful git interface
 return {
-  "NeogitOrg/neogit",
-  lazy = true,
-  dependencies = {
-    "nvim-lua/plenary.nvim",  -- required
-    "sindrets/diffview.nvim", -- optional
-    "m00qek/baleia.nvim",     -- optional
-    "folke/snacks.nvim",      -- optional
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional
+      "m00qek/baleia.nvim", -- optional
+      "folke/snacks.nvim", -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
   },
-  cmd = "Neogit",
-  keys = {
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      enhanced_diff_hl = true,
+      hooks = {
+        diff_buf_win_enter = function(bufnr, winid, ctx)
+          if ctx.symbol == "a" then
+            -- Left panel (old/removed side): use red-tinted inline highlight
+            vim.api.nvim_set_option_value(
+              "winhighlight",
+              "DiffAdd:DiffviewDiffAddAsDelete,DiffDelete:DiffviewDiffDeleteDim,DiffChange:DiffviewDiffChangeDelete,DiffText:DiffviewDiffTextDelete",
+              { win = winid }
+            )
+          end
+        end,
+      },
+    },
   },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,24 @@
 return {
   -- Use gruvbox theme
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      overrides = {
+        -- right panel: line-level background for changed lines (green-tinted)
+        DiffChange = { bg = "#2e3b1f" },
+        -- right panel: inline changed words (brighter green)
+        DiffText = { bg = "#4d6827" },
+        -- left panel: line-level background for changed lines (red-tinted)
+        DiffviewDiffChangeDelete = { bg = "#3c1f1f" },
+        -- left panel: line-level background for fully removed lines
+        DiffviewDiffAddAsDelete = { bg = "#3c1f1f" },
+        -- left panel: inline changed words (brighter red)
+        DiffviewDiffTextDelete = { bg = "#6b2e2e" },
+        -- filler lines (dimmed)
+        DiffviewDiffDeleteDim = { fg = "#5c4a4a" },
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
